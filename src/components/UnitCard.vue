9b2f6ffe8149b8ab98394ee92561cb67b04af363
Gabriel <prevosto.gabriel@neuf.fr>
2020-10-17 17:53:57 +0200

    Added basic unit page

---
+++ src/components/UnitCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-card class="my-card">
+    <q-card-section horizontal>
+      <q-img
+        class="col-5"
+        src="https://cdn.quasar.dev/img/parallax1.jpg"
+      />
+      <q-card-section
+        vertical
+        class="col"
+      >
+        <div class="row justify-between">
+          <div>
+            {{unit.name}}
+          </div>
+          <div>
+            {{unit.owner}}
+          </div>
+        </div>
+        <div>
+          HP : {{unit.hpCur}}/{{unit.hpMax}}
+        </div>
+        <div class="row">
+          <div class="col">
+            ATQ : {{unit.atk}}
+          </div>
+          <div class="col">
+            VIT : {{unit.vit}}
+          </div>
+          <div class="col">
+            TEC : {{unit.tec}}
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            DEF : {{unit.def}}
+          </div>
+          <div class="col">
+            RES : {{unit.res}}
+          </div>
+          <div class="col">
+            CHA : {{unit.cha}}
+          </div>
+        </div>
+      </q-card-section>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: ['unit']
+}
+</script>
---
+++ src/pages/UnitPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <unit-card :unit="unit" />
+</template>
+
+<script>
+const unit = {
+  name: 'Ike',
+  owner: 'Gabriel',
+  avatar: 'https://cdn.quasar.dev/img/parallax1.jpg',
+  hpCur: 20,
+  hpMax: 20,
+  atk: 20,
+  vit: 20,
+  tec: 20,
+  def: 20,
+  res: 20,
+  cha: 20,
+}
+
+export default {
+  data () {
+    return {
+      unit
+    }
+  },
+  components: {
+    'unit-card': require('components/UnitCard.vue').default
+  }
+}
+</script>
