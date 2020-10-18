73720e89d9fe497ed712f52dd62ec25042aca56e
Gabriel <prevosto.gabriel@neuf.fr>
2020-10-18 10:22:37 +0200

    Removed default unit

--- src/pages/UnitPage.vue
+++ src/pages/UnitPage.vue
@@ -5,20 +5,6 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 
-const unit = {
-  name: 'Ike',
-  owner: 'Gabriel',
-  avatar: 'https://cdn.quasar.dev/img/parallax1.jpg',
-  hpCur: 20,
-  hpMax: 20,
-  atk: 20,
-  vit: 20,
-  tec: 20,
-  def: 20,
-  res: 20,
-  cha: 20,
-}
-
 export default {
   components: {
     'unit-card': require('components/UnitCard.vue').default
