91349348a6944b72769f375dbefeb0cbb2caf82d
Gabriel <prevosto.gabriel@neuf.fr>
2020-10-27 21:22:54 +0100

    Fixes and refactoring

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <q-item class="q-pa-none q-ma-none">
+  <q-item
+    class="q-pa-none q-ma-none"
+    v-if="skill"
+  >
     <q-item-section avatar>
       <q-avatar v-if="skill.icon">
         <img :src="skill.icon" />
@@ -25,29 +28,12 @@
         v-if="skill.type === 'styles'"
       >
         <div class="row">
-          <div class="col-3">
-            FOR : {{skill.stats.str}}
-          </div>
-          <div class="col-3">
-            MAG : {{skill.stats.mag}}
-          </div>
-          <div class="col-3">
-            VIT : {{skill.stats.spd}}
-          </div>
-          <div class="col-3">
-            TEC : {{skill.stats.skl}}
-          </div>
-          <div class="col-3">
-            DEF : {{skill.stats.def}}
-          </div>
-          <div class="col-3">
-            RES : {{skill.stats.res}}
-          </div>
-          <div class="col-3">
-            CHA : {{skill.stats.lck}}
-          </div>
-          <div class="col-3">
-            MOV : {{skill.stats.mov?skill.stats.mov:0}}
+          <div
+            class="col-3"
+            v-for="(statName, statKey) in statNames"
+            :key="statKey"
+          >
+            {{statName}} : {{skill.stats[statKey]?skill.stats[statKey]:0}}
           </div>
         </div>
       </q-item-label>
@@ -60,6 +46,20 @@ import { mapState, mapActions } from 'vuex'
 
 export default {
   props: ['skillKey'],
+  data () {
+    return {
+      statNames: {
+        str: "FOR",
+        mag: "MAG",
+        spd: "VIT",
+        skl: "TEC",
+        def: "DEF",
+        res: "RES",
+        lck: "CHA",
+        mov: "MOV",
+      }
+    }
+  },
   computed: {
     ...mapState('UserStore', ['userDetails']),
     ...mapState('StaticStore', ['skills']),
